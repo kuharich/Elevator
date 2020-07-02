47ca2b562febaed0064f5f48142536d9136e364a
MARK KUHARICH <kuharich@hotmail.com>
2020-07-01 20:52:08 -0700

    Initial Commit

---
+++ Elevator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Elevator
+//
+//  Created by Mark Kuharich on 7/1/20.
+//  Copyright © 2020 Mark Kuharich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
